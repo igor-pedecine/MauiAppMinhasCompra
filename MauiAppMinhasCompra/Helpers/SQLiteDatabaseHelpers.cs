4931c0663288ccda1cb3530d669c927580a44824
Igor Pedecine Leal <igorpedleal@gmail.com>
2026-02-26 18:46:39 -0300

    Construção de Classe para fazer CRUD no SQLite

--- MauiAppMinhasCompra/Helpers/SQLiteDatabaseHelpers.cs
+++ MauiAppMinhasCompra/Helpers/SQLiteDatabaseHelpers.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppMinhasCompra.Models;
+using SQLite;
 
 namespace MauiAppMinhasCompra.Helpers
 {
-    internal class SQLiteDatabaseHelpers
+    public class SQLiteDatabaseHelpers
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelpers(string caminho) 
+        {
+            _conn = new SQLiteAsyncConnection(caminho);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> Insert(Produto p) 
+        { 
+            return _conn.InsertAsync(p);
+        }
+        public Task<List<Produto>> Update(Produto p) 
+        {
+            string sql = "UPDADTE Produto SET Descricao=?, Quantidade=?, Preco=?, WHERE Id=?";
+
+            return _conn.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade, p.Preco, p.Id
+            );
+        }
+        public Task<int> Delete(int id) 
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+        public Task<List<Produto>> Search(string q) 
+        {
+            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+
+            return _conn.QueryAsync<Produto>(sql);
+        }
     }
 }
