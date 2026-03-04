db2a4d32ba4249beb1c9ef0f99d44a6610e4b7af
Igor Pedecine Leal <igorpedleal@gmail.com>
2026-03-04 17:16:35 -0300

    atualização do insert e o botão de salvar novo produto

--- MauiAppMinhasCompra/App.xaml.cs
+++ MauiAppMinhasCompra/App.xaml.cs
@@ -1,7 +1,32 @@
-﻿namespace MauiAppMinhasCompra
+﻿using __XamlGeneratedCode__;
+using MauiAppMinhasCompra.Helpers;
+
+namespace MauiAppMinhasCompra
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelpers _db;
+        
+        public static SQLiteDatabaseHelpers Db 
+        {
+            get 
+            {
+                if (_db == null)
+                {
+
+                    string caminho = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelpers(caminho);
+                }
+                return _db;
+            }
+        
+        
+        
+        }
         public App()
         {
             InitializeComponent();
--- MauiAppMinhasCompra/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompra/Views/NovoProduto.xaml.cs
@@ -1,3 +1,4 @@
+using MauiAppMinhasCompra.Models;
 namespace MauiAppMinhasCompra.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +7,23 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto p = new Produto()
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+            App.Db.Insert(p);
+            DisplayAlert("Sucesso!!", "Registro cadastrado com sucesso!!", "OK");
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops!!", ex.Message, "OK");
+        }
+    }
 }
